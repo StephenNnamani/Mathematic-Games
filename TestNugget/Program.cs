4acf6c719cffec773e73fbb95f7294f76b33c1e0
StephenNnamani <sir.stephennnamani@gmail.com>
2023-01-15 10:38:53 +0100

    Update on Sunday

---
+++ TestNugget/Program.cs
@@ -0,0 +1,16 @@
+﻿using Country_And_States_Telephone_Codes;
+
+namespace TestNugget
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            CountryCodes countryCodes = new CountryCodes();
+
+            string result = countryCodes.GetCountryCodes("Nigeria");
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ TestNuggetMain/Program.cs
@@ -0,0 +1,13 @@
+﻿using Country_And_States_Telephone_Codes;
+
+namespace TestNuggetMain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = CountryCodes.GetACountryCode("Nigeria");
+            Console.WriteLine(result);
+        }
+    }
+}
